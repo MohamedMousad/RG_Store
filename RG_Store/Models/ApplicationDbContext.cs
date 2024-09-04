f2a75c41f7c297e1e0f63982a21e5481ffdf8cad
Mohamed Mousad <ammousad@gmail.com>
2024-09-04 15:15:16 +0300

    changed pro file to RG_Store

--- RG_Store/Models/ApplicationDbContext.cs
+++ RG_Store/Models/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace Pro.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-8OO1758\\SQLEXPRESS;Database=Product;Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("Server=.;Database=Product;Trusted_Connection=True;TrustServerCertificate=True;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
