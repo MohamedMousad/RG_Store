02a0c76c5313e52cd65b8eb3ef920733162250c6
Mohamed-Elbhairy <mohamedelbhairy.work@gmail.com>
2024-09-07 23:39:26 +0300

    test

--- RG_Store.DAL/DB/ApplicationDbContext.cs
+++ RG_Store.DAL/DB/ApplicationDbContext.cs
@@ -6,7 +6,11 @@ namespace RG_Store.DAL.DB
         {
             public DbSet<Item> Items { get; set; }
             public DbSet<Order> Orders { get; set; }
-            public DbSet<User> Users { get; set; }
+            public DbSet<User> Users { get; set; } 
+        /// <summary>
+        /// aa
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
 
             protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             {
