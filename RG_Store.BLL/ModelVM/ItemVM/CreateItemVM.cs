faa74f4706a441d9bd7688ab659c2d9381984ea2
Mohamed Mousad <ammousad@gmail.com>
2024-09-12 21:54:45 +0300

    latest fix and adding mapper

--- RG_Store.BLL/ModelVM/ItemVM/CreateItemVM.cs
+++ RG_Store.BLL/ModelVM/ItemVM/CreateItemVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace RG_Store.BLL.ModelVM.ItemVM
 {
     public class CreateItemVM
     {
+        public IFormFile Image { get; set; }
     }
 }
