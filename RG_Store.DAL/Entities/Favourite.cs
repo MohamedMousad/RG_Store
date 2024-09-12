400e311d2d80e49174ac2d51f8623a0e84ef1fd3
Mohamed-Elbhairy <mohamedelbhairy.work@gmail.com>
2024-09-12 22:11:24 +0300

    fix2

--- RG_Store.DAL/Entities/Favourite.cs
+++ RG_Store.DAL/Entities/Favourite.cs
@@ -14,7 +14,7 @@ namespace RG_Store.DAL.Entities
         public int Id { get; set; }
         public IEnumerable<Item?>? Items { get; set; }
       //  public string UserId { get; set; }
-        public User User { get; set; }
+        public User ?User { get; set; }
 
     }
 }
