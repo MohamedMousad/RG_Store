ef6d713b53647b1c620f0e9d090cc7df07187e79
Mohamed-Elbhairy <mohamedelbhairy.work@gmail.com>
2024-09-14 00:36:19 +0300

    Link SignIn & SignUp

--- RG_Store.BLL/Service/Abstraction/ICartService.cs
+++ RG_Store.BLL/Service/Abstraction/ICartService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RG_Store.BLL.ModelVM.ItemVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace RG_Store.BLL.Service.Abstraction
 {
     public interface ICartService
     {
-
+        public bool AddToCart(int ItemId, int id);
+        public bool RemoveFromCart(int ItemId, int id);
+        public IEnumerable<GetAllItemVM> GetAll(int id);
+        public bool ClearCart(int id);
+        public decimal GetCartPrice(int id);
     }
 }
