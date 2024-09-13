890d3c99a8751c79d248abe91bef7725bb21ca05
Mohamed-Elbhairy <mohamedelbhairy.work@gmail.com>
2024-09-13 21:36:45 +0300

    Add SignIn & SignUp View With Style

--- RG_Store.BLL/Service/Abstraction/IItemService.cs
+++ RG_Store.BLL/Service/Abstraction/IItemService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entities;
+using RG_Store.BLL.ModelVM.ItemVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace RG_Store.BLL.Service.Abstraction
 {
     public interface IItemService
     {
+        public bool Create(CreateItemVM createItemVM);
+        public bool Update(UpdateItemVM updateItem);
+        public bool Delete(DeleteItemVM deleteItem);
+        public bool GetAllItem(GetAllItemVM deleteItem);
+        T GetById<T>(int id, Func<Item, T> converter);
+        public IEnumerable<GetAllItemVM> GetAll();
     }
 }
--- RG_Store.BLL/Service/Implementation/ItemService.cs
+++ RG_Store.BLL/Service/Implementation/ItemService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Entities;
+using RG_Store.BLL.ModelVM.ItemVM;
+using RG_Store.BLL.Service.Abstraction;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace RG_Store.BLL.Service.Implementation
 {
-    public class ItemService
+    public class ItemService : IItemService
     {
+        public bool Create(CreateItemVM createItemVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(DeleteItemVM deleteItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GetAllItemVM> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetAllItem(GetAllItemVM deleteItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById<T>(int id, Func<Item, T> converter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(UpdateItemVM updateItem)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
