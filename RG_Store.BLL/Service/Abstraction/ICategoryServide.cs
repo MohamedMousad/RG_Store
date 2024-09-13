ea199bf58c2e46b4aa66ec1ff472086108bddce5
Mohamed-Elbhairy <mohamedelbhairy.work@gmail.com>
2024-09-14 00:02:08 +0300

    Add CategoryService

--- RG_Store.BLL/Service/Abstraction/ICategoryServide.cs
+++ RG_Store.BLL/Service/Abstraction/ICategoryServide.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RG_Store.BLL.ModelVM.Category;
+using RG_Store.DAL.Migrations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace RG_Store.BLL.Service.Abstraction
 {
     public interface ICategoryServide
     {
+        public bool Create(AddCategoryVM CategoryVM);
+        public GetCategoryVM Get(int id);
+        public bool Update(UpdateCategoryVM CategoryVM);
+        public bool Delete(DeleteCategoryVM CategoryVM);
+        public IEnumerable<GetCategoryVM> GetAll();
     }
 }
--- RG_Store.BLL/Service/Implementation/CategoryServide.cs
+++ RG_Store.BLL/Service/Implementation/CategoryServide.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using AutoMapper;
+using EmployeeSystem.DAL.Repo.Implementation;
+using Entities;
+using RG_Store.BLL.ModelVM.Category;
+using RG_Store.BLL.ModelVM.ItemVM;
+using RG_Store.BLL.Service.Abstraction;
+using RG_Store.DAL.Entities;
+using RG_Store.DAL.Repo.Abstraction;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +14,51 @@ using System.Threading.Tasks;
 
 namespace RG_Store.BLL.Service.Implementation
 {
-    public class CategoryServide
+    public class CategoryServide : ICategoryServide
     {
+        private readonly IMapper mapper;
+        private readonly ICategoryRepo categoryRepo;
+
+        public CategoryServide(ICategoryRepo categoryRepo, IMapper mapper)
+        {
+            this.categoryRepo = categoryRepo;
+            this.mapper = mapper;
+        }
+
+        public bool Create(AddCategoryVM CategoryVM)
+        {
+            var Result = mapper.Map<Category>(CategoryVM);
+            return categoryRepo.Create(Result);
+        }
+
+        public bool Delete(DeleteCategoryVM CategoryVM)
+        {
+            var Result = mapper.Map<Category>(CategoryVM);
+            return categoryRepo.Delete(Result);
+        }
+
+        public GetCategoryVM Get(int id)
+        {
+            var Item = categoryRepo.GetById(id);
+            var Result = mapper.Map<GetCategoryVM>(Item);
+            return Result;
+        }
+
+        public IEnumerable<GetCategoryVM> GetAll()
+        {
+            var List = categoryRepo.GetAll().ToList();
+            List<GetCategoryVM> Result = new();
+            foreach (var item in List)
+            {
+                var temp = mapper.Map<GetCategoryVM>(item);
+                Result.Add(temp);
+            }
+            return Result;
+        }
+        public bool Update(UpdateCategoryVM CategoryVM)
+        {
+            var Result = mapper.Map<Category>(CategoryVM);
+            return categoryRepo.Update(Result);
+        }
     }
 }
