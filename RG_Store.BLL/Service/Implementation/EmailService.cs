6442c0e1db2f943ed67812cf212db49bd34988a9
mazen159 <mazensalh87@gmai.com>
2024-09-16 18:18:53 +0300

    EmailConfirmation

---
+++ RG_Store.BLL/Service/Implementation/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using RG_Store.BLL.Service.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RG_Store.BLL.Service.Implementation
+{
+    public class EmailService :IEmailService
+    {
+        public void SendEmail(string to, string subject, string body)
+        {
+            var smtpClient = new SmtpClient("smtp.outlook.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("rg_storeproject@outlook.com", "123456789/."),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("rg_storeproject@outlook.com"),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+            mailMessage.To.Add(to);
+
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
---
+++ RG_Store.DAL/Migrations/20240916142556_AddEmailConfirmationService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RG_Store.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailConfirmationService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailConfirmationToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailConfirmed",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmationToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailConfirmed",
+                table: "AspNetUsers");
+        }
+    }
+}
