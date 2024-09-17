9b30e85450e6eb4e85ecf4f9b3304014f17accb8
Mohamed-Elbhairy <mohamedelbhairy.work@gmail.com>
2024-09-18 01:25:45 +0300

    edit item entity

--- RG_Store.DAL/Entities/Item.cs
+++ RG_Store.DAL/Entities/Item.cs
@@ -19,8 +19,8 @@ namespace Entities
         public decimal ?Offer { get; set; } = 0;
         public bool IsDeleted { get; set; } = false;
         public int? CategoryId { get; set; }
-        public IEnumerable<CartItem?> CartItem { get; set; }
-        public IEnumerable<FavouriteItem?> FavouriteItem { get; set; }
+    /*    public IEnumerable<CartItem?> CartItem { get; set; }
+        public IEnumerable<FavouriteItem?> FavouriteItem { get; set; }*/
         public Category? Category { get; set; }  
         public String ? Image { get; set; }
 
